6c73082a74713b750c3be963221c13e379fd0f16
studiominti <studiominti@gmail.com>
2020-04-26 21:17:27 -0400

    Dark Mode state by Vuex and browser color detection feature for toggle app colors.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+
+
 export default {
   name: "App",
   mounted() {
@@ -16,6 +18,10 @@ export default {
 </script>
 
 <style lang="scss">
+
+* {
+  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+}
 
 body {
   background: $dark-blue;
