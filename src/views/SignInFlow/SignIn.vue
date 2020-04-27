79b210a2410fbc627d5b40c5cdabbc81264a55a3
studiominti <studiominti@gmail.com>
2020-04-26 22:50:05 -0400

    Toggle switch for Ligh-Dark mode and some SASS optimization

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="container" :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}">
-    <div class="request" :class="{'light-request' : isDarkMode, 'dark-request' : !isDarkMode}">
-      Don't have an account?
-      <router-link to="/request">Request an account here
-      </router-link>
-    </div>
+    <RequestAccount/>
     <div class="login">
       <img src="@/assets/logo.png">
       <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Sign into my first Vue App</h4>
@@ -13,90 +9,31 @@
       <button>Sign In</button>
       <router-link to="/recover" :class="{'light-link': isDarkMode, 'dark-link' : !isDarkMode}">Forgot your password?
       </router-link>
-      <button @click="toggleDarkMode">Toggle</button>
+      <ThemeSwitch/>
     </div>
   </div>
 </template>
 
 <script>
+
+import RequestAccount from "@/components/RequestAccount";
+import ThemeSwitch from "@/components/ThemeSwitch";
+
   export default {
     name: "SignIn",
+    components: {
+      RequestAccount,
+      ThemeSwitch
+    },
     computed: {
       isDarkMode() {
         return this.$store.getters.isDarkMode
       }
-    },
-    methods: {
-      toggleDarkMode() {
-        this.$store.commit("toggleDarkMode");
-      }
-    }  
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-
-
-.light-background {
-  background-color: $light-gray;
-
-}
-
-.dark-background {
-  background-color: $dark-blue;
-}
-
-“.light-text {
-  color: $white;
-}
-
-.dark-text {
-  color: $black;
-}
-
-.light-field {
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-.dark-field {
-  background: rgba(198, 208, 235, 0.2);
-  border: 1px solid rgba(0, 0, 0, 0.2);
-
-  &::placeholder {
-    color: rgba(0, 0, 0, 0.3);
-  }
-}
-
-.light-request {
-  color: rgba(255, 255, 255, 0.3);
-
-  a {
-    color: white;
-  }
-}
-
-.dark-request {
-  color: rgba(0, 0, 0, 0.3);
-
-  a {
-    color: $black;
-  }
-}
-
-.light-link {
-  color: rgba(255, 255, 255, 0.3);
-}
-
-.dark-link {
-  color: rgba(0, 0, 0, 0.3);
-}
-
-
 
 .container  {
   display: flex;
@@ -110,64 +47,7 @@
   width: 400px;
 }
 
-.request {
-  position: absolute;
-  top: 40px;
-  right: 40px;
-
-}
-
 img {
   width: 50%;
-}
-
-h4 {
-  margin: 0;
-  line-height: 34px;
-  font-size: 24px;
-  text-align: center;
-
-  color: #ffffff;
-}
-
-input {
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  box-sizing: border-box;
-  border-radius: 4px;
-
-  height: 60px;
-  width: 100%;
-  font-size: 20px;
-  color: white;
-  padding-left: 20px;
-  margin-top: 20px;
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-button {
-  background: #56ccf2;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
-  border: none;
-
-  height: 60px;
-  width: 100%;
-
-  font-size: 20px;
-  color: #ffffff;
-  margin-top: 20px;
-  margin-bottom: 40px;
-}
-
-a {
-  line-height: 25px;
-  font-size: 16px;
-  text-align: center;
-  color: rgba(255, 255, 255, 0.3);
-  text-decoration: none; 
 }
 </style>
